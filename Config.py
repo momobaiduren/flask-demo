e2e388d3859b870b69bf28877366a0232343d07b
zhanglong <718168633@qq.com>
2020-01-02 16:00:02 +0800

    删除无用包

--- Config.py
+++
@@ -1,5 +0,0 @@
-
-class Config():
-    def __init__(self) -> None:
-        self.db_url = "mysql+pymysql://root:xxxxx@localhost:3306/test?charset=utf8"
-        self.sqlalchemy_track_modifications = True
